e2a70dfdff411999ee9662062d484e86f434117c
Mudassir <mudassir.inception@gmail.com>
2023-03-21 14:43:22 +0530

    issue :- / route issue

--- app.js
+++ app.js
@@ -17,19 +17,21 @@ const sumoQuoteRoutes = require("./routes/sumoquote");
 app.use("/hubspot", hubSpotRoutes);
 app.use("/sumoquote", sumoQuoteRoutes);
 
-app.use('/documentation', async (req, res) => {
+app.use('/public', express.static(path.join(__dirname, 'public')))
+
+
+app.get('/documentation', async (req, res) => {
   res.render('pages/documentation');
 });
 
-app.use('/terms-and-conditions', async (req, res) => {
+app.get('/terms-and-conditions', async (req, res) => {
   res.render('pages/terms-and-conditions');
 });
 
-app.use('/', async (req, res) => {
+app.get('/', async (req, res) => {
   res.render('pages/documentation');
 });
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => console.log(`Server running on ${process.env.HOST}`));
 
