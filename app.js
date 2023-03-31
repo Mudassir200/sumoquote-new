4792439b779f7fe6da55eecffdfe96a4a545304a
Mudassir <ayaz@roofingbusinesspartner.com>
2023-03-31 12:54:56 +0530

    cache issue solve

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const nocache = require("nocache");
 require("dotenv").config();
 require('./db/index')();
 const PORT = process.env.PORT || 4050;
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 4050;
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({limit: '50mb', extended: true }))
 app.use(express.json({limit: '50mb'}))
-
+app.use(nocache());
 
 const hubSpotRoutes = require("./routes/hubspot");
 const sumoQuoteRoutes = require("./routes/sumoquote");
